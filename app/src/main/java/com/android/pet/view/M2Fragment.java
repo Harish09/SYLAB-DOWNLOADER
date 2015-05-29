55e5899953d40deef2cb4c030c0cdcc1a4d7fb76
faizaanceg <aulisius7@gmail.com>
2015-05-29 22:24:25 +0530

    PDFViewer added

--- app/src/main/java/com/android/pet/view/M2Fragment.java
+++ app/src/main/java/com/android/pet/view/M2Fragment.java
@@ -8,6 +8,10 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.doepiccoding.navigationdrawer.R;
+import com.joanzapata.pdfview.PDFView;
+
+import java.io.File;
+
 
 public class M2Fragment extends Fragment {
 
@@ -18,17 +22,26 @@ public class M2Fragment extends Fragment {
         //TextView.class.cast(rootView.findViewById(R.id.labelText)).setText("Venus");
         String url = "http://cs.annauniv.edu/academic/ug2012/M2.pdf";
 
+        PDFView pdfView = (PDFView) rootView.findViewById(R.id.pdfView);
+
         FileDownloader fileDownloader = new FileDownloader();
+        File file = new File(fileDownloader.getFilePath("/SYLAB/M2.pdf"));
 
         if(fileDownloader.isReadyForDownload(null, this, "/SYLAB/M2.pdf")) {
             fileDownloader.DownloadSyllabus(getActivity().getApplicationContext(), url, "M2");
+
         }
         else {
             if(!fileDownloader.getmConnected()) {
                 Toast.makeText(getActivity().getApplicationContext(), "Network error", Toast.LENGTH_SHORT).show();
+                if(fileDownloader.isFileAlreadyPresent("/SYLAB/M2.pdf")) {
+                    pdfView.fromFile(file).defaultPage(1).enableSwipe(true).load();
+                }
             }
             if(!fileDownloader.getmFileStatus()) {
                 Toast.makeText(getActivity().getApplicationContext(), "File already downloaded", Toast.LENGTH_SHORT).show();
+
+                pdfView.fromFile(file).defaultPage(1).enableSwipe(true).load();
             }
         }
 
