89970cb7a16d3817d2d3ef94589f33e64a4a7920
faizaanceg <aulisius7@gmail.com>
2015-05-31 21:35:10 +0530

    New image for home and clean up

--- app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
+++ app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
@@ -18,13 +18,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int heart=0x7f020000;
-        public static final int home=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int phome=0x7f020003;
+        public static final int home=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
+        public static final int action_settings=0x7f09000b;
         public static final int appname=0x7f090002;
         public static final int content_frame=0x7f090005;
         public static final int drawer_layout=0x7f090004;
@@ -33,7 +31,6 @@ public final class R {
         public static final int lblListItem=0x7f090008;
         public static final int lvExp=0x7f090006;
         public static final int open_btn=0x7f090003;
-        public static final int open_pdf=0x7f09000b;
         public static final int pdfView=0x7f090009;
         public static final int s1=0x7f090000;
         public static final int s9=0x7f09000a;
@@ -50,8 +47,7 @@ public final class R {
         public static final int third=0x7f030008;
     }
     public static final class menu {
-        public static final int fragment=0x7f080000;
-        public static final int navigation=0x7f080001;
+        public static final int navigation=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f060000;
