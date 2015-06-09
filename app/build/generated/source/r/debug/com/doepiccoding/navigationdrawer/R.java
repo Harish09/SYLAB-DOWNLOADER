d142d91f04e71e7f1a3fd3032a959946e61dbabe
karthick <rkarthick31996@gmail.com>
2015-06-09 17:18:01 +0530

    App final changes

--- app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
+++ app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
@@ -19,7 +19,8 @@ public final class R {
         public static final int github=0x7f020001;
         public static final int home=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int sylab=0x7f020004;
+        public static final int ico=0x7f020004;
+        public static final int sylab=0x7f020005;
     }
     public static final class id {
         public static final int about=0x7f080000;
