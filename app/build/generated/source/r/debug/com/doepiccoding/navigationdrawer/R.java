15e1cc353de20002f15337750e982d4df003a056
karthick <rkarthick31996@gmail.com>
2015-06-10 22:58:27 +0530

    Trying to fix coming soon...

--- app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
+++ app/build/generated/source/r/debug/com/doepiccoding/navigationdrawer/R.java
@@ -20,7 +20,7 @@ public final class R {
         public static final int github=0x7f020002;
         public static final int home=0x7f020003;
         public static final int ic_launcher=0x7f020004;
-        public static final int ico=0x7f020005;
+        public static final int logo=0x7f020005;
         public static final int sylab=0x7f020006;
     }
     public static final class id {
